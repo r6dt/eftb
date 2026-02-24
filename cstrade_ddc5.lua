2c1859776a20cf5ed4aee2f20975a100ca0f9648
r6dt <wycswy7h@gmail.com>
2026-02-24 20:10:03 +0700

    Add files via upload

---
+++ cstrade_ddc5.lua
@@ -0,0 +1,9 @@
+repeat task.wait() until game:IsLoaded()
+task.wait(10)
+
+script_key="SkEydJsfMqlUeNExpGwUZhIkGYxPoLmn";
+getgenv().AutoTrade = {
+    Collector = "fliy54cazi39", -- Username for main account to receive gifts
+    BrainrotToGift = {"Meta Technetta", "Magmew", "Anububu"}, -- list of braintrots to gift
+}
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/33fdc0c03f51149b58774a2e1479ee85.lua"))()
