1dbffbd37ab5985fd590f692e36549af92b62281
r6dt <wycswy7h@gmail.com>
2026-02-27 18:15:00 +0700

    Update cstrade_ddc3.lua

--- cstrade_ddc3.lua
+++ cstrade_ddc3.lua
@@ -3,7 +3,7 @@ task.wait(10)
 
 script_key="pQhfWNtsKSNfuzQNakIVOxwWrzOgHpMr";
 getgenv().AutoTrade = {
-    Collector = "rbhjzbbl7660", -- Username for main account to receive gifts
+    Collector = "peqntbnm6211", -- Username for main account to receive gifts
     BrainrotToGift = {"Meta Technetta", "Magmew", "Anububu"}, -- list of braintrots to gift
 }
 loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/33fdc0c03f51149b58774a2e1479ee85.lua"))()
