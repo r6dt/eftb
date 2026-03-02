7b5d910f3c29ed0103ffa216ee021f2966a7bad4
r6dt <wycswy7h@gmail.com>
2026-03-02 11:36:18 +0700

    Update cs_ddc1.lua

--- cs_ddc1.lua
+++ cs_ddc1.lua
@@ -39,17 +39,17 @@ getgenv().Configs = {
     --=== More Farms ===--
     Farm_Shield = true, -- เปิดใช้งานฟาร์มโล่ (Farm Shield)
     Farm_Tower = true, -- เปิดใช้งานฟาร์มทาวเวอร์ (Farm Tower)
-    Keep_Tower_Rarities = {"Infinity", "Divine"}, -- รายชื่อตัวที่ต้องการเก็บจากทาวเวอร์ (Tower Brainrot Rarities to Keep)
+    Keep_Tower_Rarities = {"Infinity"}, -- รายชื่อตัวที่ต้องการเก็บจากทาวเวอร์ (Tower Brainrot Rarities to Keep)
 
     --=== Lucky Block Configs ===--
     Farm_LuckyBlock = true, -- เปิดใช้งานฟาร์มลัคกี้บล็อก (Farm Lucky Block) < 5
-    Keep_LuckyBlock = {"Infinity"}, -- รายชื่อลัคกี้บล็อกที่ต้องการเก็บ (Lucky Block Types to Keep)
+    Keep_LuckyBlock = {"Infinity", "Divine"}, -- รายชื่อลัคกี้บล็อกที่ต้องการเก็บ (Lucky Block Types to Keep)
     Open_LuckyBlock = true, -- เปิดใช้งานการเปิดลัคกี้บล็อก (Open Lucky Block)
     Keep_LuckyBlock_Rarities = {"Infinity", "Divine"}, -- รายชื่อลัคกี้บล็อกที่ไม่ต้องการเปิด (Lucky Block Rarities to Keep)
 
     --=== Auto Rebirth ==--
     MaxSpeed = 600, -- ความเร็วสูงสุด (Max Speed)
-    MaxRebirth = 25, -- รีเบิร์ทสูงสุด (Max Rebirth)
+    MaxRebirth = 30, -- รีเบิร์ทสูงสุด (Max Rebirth)
 
     --=== Log Configs ===--
     HorstLog = false, -- เปิดใช้งานการส่งข้อมูลไปยัง Horst
