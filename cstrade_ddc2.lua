9161cf56f88c2270f5692c95a69c926ac13a1485
r6dt <wycswy7h@gmail.com>
2026-03-05 19:26:19 +0700

    Update cstrade_ddc2.lua

--- cstrade_ddc2.lua
+++ cstrade_ddc2.lua
@@ -3,7 +3,7 @@ task.wait(10)
 
 script_key="MCryPdYZrfyRweLyMzMdUnHZFXexofbp";
 getgenv().AutoTrade = {
-    Collector = "ckcelzpk5788", -- Username for main account to receive gifts
+    Collector = "rkvmwdtw4689", -- Username for main account to receive gifts
     Items = {"Meta Technetta", "Magmew", "nububu", "Infinity Block"}, -- list of braintrots to gift
 }
 loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/33fdc0c03f51149b58774a2e1479ee85.lua"))()
