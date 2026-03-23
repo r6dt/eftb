8e34a35989a10e127d7adb635b06fc8633870a1f
r6dt <wycswy7h@gmail.com>
2026-03-23 21:54:52 +0700

    Update cs_speed0.lua

--- cs_speed0.lua
+++ cs_speed0.lua
@@ -1,2 +1,5 @@
 script_key="kMvfWMPPTPffdXGSmjMlbQROZlymajzG";
+getgenv().Starter = {
+    Single_Player_Mode = true, -- Need to join vip server first
+}
 loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/47943d1a5556f97f195d657e45ec2c1f.lua"))()
