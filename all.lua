996e1c009c870cf833ff1e083f098d867f70b3b3
r6dt <wycswy7h@gmail.com>
2026-03-23 20:57:05 +0700

    Update all.lua

--- all.lua
+++ all.lua
@@ -5,7 +5,7 @@ local speed = LocalPlayer:GetAttribute("CurrentSpeed")
 
 if speed then
     if speed < 400 then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/r6dt/eftb/refs/heads/main/cs_ddc1.lua"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/r6dt/eftb/refs/heads/main/cs_speed0.lua"))()
     else
         loadstring(game:HttpGet("https://raw.githubusercontent.com/r6dt/eftb/refs/heads/main/cs_speed400.lua"))()
     end
