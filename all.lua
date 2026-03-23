d6625c8d8897dd3f6a62632899540a3347095fbc
r6dt <wycswy7h@gmail.com>
2026-03-23 20:55:48 +0700

    Create all.lua

---
+++ all.lua
@@ -0,0 +1,12 @@
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+
+local speed = LocalPlayer:GetAttribute("CurrentSpeed")
+
+if speed then
+    if speed < 400 then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/r6dt/eftb/refs/heads/main/cs_ddc1.lua"))()
+    else
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/r6dt/eftb/refs/heads/main/cs_ddc2.lua"))()
+    end
+end
